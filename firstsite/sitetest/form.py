9c474713c263a406651918d90f814289f8e39401
Yaroslav <yaroslawsh04@gmail.com>
2024-01-28 16:10:53 +0100

    Created Validation Class for Title in AddPost Template Form

--- firstsite/sitetest/form.py
+++ firstsite/sitetest/form.py
@@ -1,11 +1,26 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
 
 from sitetest.models import Category, Country
 
 
+@deconstructible
+class Title_Validator:
+    ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+    code = "english"
+
+    def __init__(self, message=None):
+        self.message = message if message else "You are stupid"
+
+    def __call__(self, value, *args, **kwargs):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code)
+
+
 class CreateForm(forms.Form):
-    title = forms.CharField(max_length=255, label="Title")
+    title = forms.CharField(max_length=255, label="Title", validators=[Title_Validator()])
     content = forms.CharField(widget=forms.Textarea())
-    is_published=forms.BooleanField(required=False, label="Publish", initial=True)
+    is_published = forms.BooleanField(required=False, label="Publish", initial=True)
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Another Category")
     country = forms.ModelChoiceField(queryset=Country.objects.all(), required=False, empty_label="Not Listed")
