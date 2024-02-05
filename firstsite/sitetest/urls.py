771e380c2395f2e78a7936316ea4d15f4713088c
Yaroslav <yaroslawsh04@gmail.com>
2024-02-05 15:19:43 +0100

    Changed view functions show_tags and show_cats to view classes ShowTags and ShowCats. Added dynamic data processing

--- firstsite/sitetest/urls.py
+++ firstsite/sitetest/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('', views.Persons_Main.as_view(), name="home"),
     path('about/', views.about_index, name="about"),
     path('post/<slug:post_slug>/', views.show_post, name="post"),
-    path('category/<slug:cat_slug>/', views.show_categories, name="category"),
+    path('category/<slug:cat_slug>/', views.ShowCategories.as_view(), name="category"),
     path('addpost', views.addpost, name='add_post'),
-    path('tags/<slug:tag_slug>/', views.show_tags, name="tags"),
+    path('tags/<slug:tag_slug>/', views.ShowTags.as_view(), name="tags"),
 ]
 
 if settings.DEBUG:
--- firstsite/sitetest/views.py
+++ firstsite/sitetest/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
-from django.views.generic import TemplateView
+from django.views import View
+from django.views.generic import TemplateView, ListView
 
 from sitetest.form import CreateForm, UploadFileForm
 from sitetest.models import Persons, Category, TagPost, FileModel
@@ -25,12 +26,14 @@ menu = [{'title': "About", 'url_name': 'about'},
 #     return render(request, "sitetest/index.html", context=data)
 
 
-class Persons_Main(TemplateView):
+class Persons_Main(ListView):
     template_name = "sitetest/index.html"
-    extra_context = {"title": "Mywebsite",
-                     "number": 220,
-                     "posts": Persons.published.all().select_related('cat'),
+    context_object_name = 'posts'
+    extra_context = {"title": "WW2 Persons",
                      "menu": menu, }
+
+    def get_queryset(self):
+        return Persons.published.all()
 
 
 def about_index(request):
@@ -86,34 +89,66 @@ def show_post(request, post_slug):
     return render(request, "sitetest/post.html", context=data)
 
 
-def show_categories(request, cat_slug):
-    category = get_object_or_404(Category, slug=cat_slug)
-    post = Persons.published.filter(cat_id=category.pk).select_related('cat')
-    data = {
-        "name": category.name,
-        "slug": category.slug,
-        "posts": post,
-        "menu": menu,
-        "cat_selected": category.pk,
-        "gay": True,
+# def show_categories(request, cat_slug):
+#     category = get_object_or_404(Category, slug=cat_slug)
+#     post = Persons.published.filter(cat_id=category.pk).select_related('cat')
+#     data = {
+#         "name": category.name,
+#         "slug": category.slug,
+#         "posts": post,
+#         "menu": menu,
+#         "cat_selected": category.pk,
+#         "gay": True,
+#
+#     }
+#
+#     return render(request, "sitetest/index.html", context=data)
 
-    }
 
-    return render(request, "sitetest/index.html", context=data)
+class ShowCategories(ListView):
+    template_name = 'sitetest/index.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Persons.published.filter(cat__slug=self.kwargs['cat_slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat = context['posts'][0].cat  # cat is name of the Category of the first queryset value
+        context['title'] = cat.name + 's'
+        context['menu'] = menu
+        context['cat_selected'] = cat.id
+        return context
 
 
-def show_tags(request, tag_slug):
-    tag = get_object_or_404(TagPost, slug=tag_slug)
-    posts = tag.tags.filter(is_published=Persons.Status.PUBLISHED)
+# def show_tags(request, tag_slug):
+#     tag = get_object_or_404(TagPost, slug=tag_slug)
+#     posts = tag.tags.filter(is_published=Persons.Status.PUBLISHED)
+#
+#     data = {
+#         "title": tag.tag,
+#         "posts": posts,
+#         "menu": menu,
+#         "cat_selected": None
+#
+#     }
+#     return render(request, "sitetest/index.html", context=data)
 
-    data = {
-        "title": tag.tag,
-        "posts": posts,
-        "menu": menu,
-        "cat_selected": None
 
-    }
-    return render(request, "sitetest/index.html", context=data)
+class ShowTags(ListView):
+    template_name = 'sitetest/index.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag = get_object_or_404(TagPost, slug=self.kwargs['tag_slug'])
+        return tag.tags.filter(is_published=Persons.Status.PUBLISHED)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag = context['posts'][0].tags.all()[0].tag  # tag is str name of a tag from first queryset value
+        context['title'] = "TAG " + tag
+        context['menu'] = menu
+        return context
 
 
 def index2(request, id):
