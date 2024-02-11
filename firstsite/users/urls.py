8a2923c01ced45803be2c4d33d9f5aaa1498042f
Yaroslav <yaroslawsh04@gmail.com>
2024-02-11 21:52:45 +0100

    Specified login.html with better visual. Logout and Login changed to classes. Little change in form

--- firstsite/users/urls.py
+++ firstsite/users/urls.py
@@ -1,11 +1,11 @@
-
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
 app_name='users'
 urlpatterns = [
     path('', views.base, name="base"),
-    path('login/', views.login_users, name="login"),
-    path('logout/', views.logout_users, name="logout"),
+    path('login/', views.LoginUsers.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 
 ]
--- firstsite/users/views.py
+++ firstsite/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
@@ -12,22 +13,29 @@ def base(request):
 
 
 # Create your views here.
-def login_users(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'],
-                                password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-    else:
-        form = LoginForm()
+# def login_users(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd['username'],
+#                                 password=cd['password'])
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect(reverse('home'))
+#     else:
+#         form = LoginForm()
+#
+#     return render(request, 'users/login.html', {'form': form})
 
-    return render(request, 'users/login.html', {'form': form})
+class LoginUsers(LoginView):
+    form_class = LoginForm
+    template_name = 'users/login.html'
 
+    def get_success_url(self):
+        return reverse_lazy('home')
 
-def logout_users(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('users:login'))
+#
+# def logout_users(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse('users:login'))
