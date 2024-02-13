6e894b813c4b00ba1831e0a91def297b88e46a12
Yaroslav <yaroslawsh04@gmail.com>
2024-02-13 15:46:14 +0100

    Register Function changed to class| Registration form in users/forms.py now Inherited from UserCreationForm

--- firstsite/users/forms.py
+++ firstsite/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class LoginForm(AuthenticationForm):
@@ -12,28 +12,27 @@ class LoginForm(AuthenticationForm):
         fields = ['username', 'passsword']
 
 
-class RegistrationForm(forms.ModelForm):
+class RegistrationForm(UserCreationForm):
     username = forms.CharField(label="Create username", widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label="Create password", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label="Create password", widget=forms.TextInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label="Repeat password", widget=forms.TextInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
         labels = {
             'email': "E-mail address",
             'first_name': "First name",
             'last_name': "Last name"
         }
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-input'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-input'})
+        }
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError("Password do not match")
-        return cd['password2']
-
-    def clean_email(self):
-        email_form = self.cleaned_data['email']
-        if get_user_model().objects.filter(email=email_form).exists():
-            raise forms.ValidationError("Email already taken")
-        return email_form
+        def clean_email(self):
+            email_form = self.cleaned_data['email']
+            if get_user_model().objects.filter(email=email_form).exists():
+                raise forms.ValidationError("Email already taken")
+            return email_form
--- firstsite/users/views.py
+++ firstsite/users/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
 
 from users.forms import LoginForm, RegistrationForm
 
@@ -26,6 +27,7 @@ class LoginUsers(LoginView):
     form_class = LoginForm
     template_name = 'users/login.html'
 
+
     # def get_success_url(self):
     #     return reverse_lazy('home')
 
@@ -35,15 +37,23 @@ class LoginUsers(LoginView):
 #     logout(request)
 #     return HttpResponseRedirect(reverse('users:login'))
 
+class RegisterUser(CreateView):
+    form_class = RegistrationForm
+    template_name = 'users/register.html'
+    extra_context = {'title': "Registration"}
+    success_url = reverse_lazy('users:login')
 
-def register_user(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    else:
-        form = RegistrationForm()
-    return render(request, 'users/register.html', context={'form': form})
+
+
+
+# def register_user(request):
+#     if request.method == 'POST':
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.set_password(form.cleaned_data['password'])
+#             user.save()
+#             return render(request, 'users/register_done.html')
+#     else:
+#         form = RegistrationForm()
+#     return render(request, 'users/register.html', context={'form': form})
